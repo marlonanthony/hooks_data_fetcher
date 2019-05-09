3d7c756a64969596526dcd4090d926df897ffb3e
marlonanthony <mad1083@gmail.com>
2019-05-08 23:51:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useReducer } from 'react'
+import axios from 'axios' 
+import './App.css'
 
 function App() {
+  const [query, setQuery] = useState('redux') 
+  const { data, isLoading, isError, doFetch } = useDataApi(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+    { hits: [] }
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => {
+        doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        e.preventDefault() 
+      }}>
+        <input 
+          type='text'
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+        />
+        <button type='button'>
+          Search
+        </button>
+      </form>
+      { isError && <div>Something went wrong...</div> }
+      { isLoading 
+        ? ( <div>Loading...</div> ) 
+        : (
+            <ul>
+              {data.hits.map(item => (
+                <li key={item.objectID}>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+              ))}
+            </ul>
+          )
+      }
     </div>
   );
+}
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return { 
+        ...state,
+        isLoading: true,
+        isError: false  
+      }
+    case 'FETCH_SUCCESS':
+      return { 
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload 
+      }
+    case 'FETCH_FAILURE':
+      return { 
+        ...state,
+        isLoading: false,
+        isError: true  
+      }
+    default: 
+      throw new Error() 
+  }
+}
+
+const useDataApi = (initialUrl, initialData) => {
+  const [url, setUrl] = useState(initialUrl) 
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      dispatch({ type: 'FETCH_INIT' })
+
+      try {
+        const result = await axios(url)
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+      } catch(error) {
+        dispatch({ type: 'FETCH_FAILURE' })
+      }
+    }
+
+    fetchData() 
+  }, [url])
+
+  const doFetch = url => setUrl(url)
+
+  return { ...state, doFetch }
 }
 
 export default App;
