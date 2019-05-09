e84ff18ea7605c2f747f56879d5e489e1792181c
marlonanthony <mad1083@gmail.com>
2019-05-09 00:34:35 -0400

    refactor

--- src/UseDataApi.js
+++ src/UseDataApi.js
@@ -5,40 +5,40 @@ import { dataFetchReducer } from './dataFetchReducer'
 function UseDataApi (initialUrl, initialData) {
     const [url, setUrl] = useState(initialUrl) 
     const [state, dispatch] = useReducer(dataFetchReducer, {
-      isLoading: false,
-      isError: false,
-      data: initialData
+        isLoading: false,
+        isError: false,
+        data: initialData
     })
-  
-    useEffect(() => {
-      let didCancel = false 
-  
-      async function fetchData() {
-        dispatch({ type: 'FETCH_INIT' })
-  
-        try {
-          const result = await axios(url)
-  
-          if(!didCancel){
-            dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
-          }
-        } catch(error) {
-          if(!didCancel) {
-            dispatch({ type: 'FETCH_FAILURE' })
-          }
-        }
-      }
-  
-      fetchData() 
-  
-      return () => {
-        didCancel = true 
-      }
-    }, [url])
-  
-    const doFetch = url => setUrl(url)
-  
-    return { ...state, doFetch }
-  }
 
-  export default UseDataApi
+    useEffect(() => {
+        let didCancel = false 
+
+        async function fetchData() {
+            dispatch({ type: 'FETCH_INIT' })
+
+            try {
+                const result = await axios(url)
+
+                if(!didCancel){
+                    dispatch({ type: 'FETCH_SUCCESS', payload: result.data })
+                }
+            } catch(error) {
+                if(!didCancel) {
+                    dispatch({ type: 'FETCH_FAILURE' })
+                }
+            }
+        }
+
+        fetchData() 
+
+        return () => {
+            didCancel = true 
+        }
+    }, [url])
+
+    const doFetch = url => setUrl(url)
+
+    return { ...state, doFetch }
+}
+
+export default UseDataApi
